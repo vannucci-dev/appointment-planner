a347b849055bcba04c65cf073f29d7df4023460f
Lorenzo Vannucci <info@vannucci-dev.com>
2021-04-13 14:30:12 +0100

    add state to App.js and create callback functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const { appointments, setAppointments } = useState([]);
+  const { contacts, setContacts } = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addAppointment = (appointment) => {
+    if (
+      appointment.title &&
+      appointment.contact &&
+      appointment.date &&
+      appointment.time
+    ) {
+      setAppointments([...appointments, appointment]);
+    }
+  };
+
+  const addContact = (contact) => {
+    if (contact.name && contact.phone && contact.email) {
+      setContacts([...contacts, contact]);
+    }
+  };
 
   return (
     <>
@@ -36,12 +46,13 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage
+              appointments={appointments}
+              addAppointment={addAppointment}
+            />
           </Route>
         </Switch>
       </main>
